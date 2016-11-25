9c93cf2c137f002af214f2badb9ae4aa83d38407
Mahamudul-Nobi-Mohon <mn.mohon@gmail.com>
2016-11-25 14:29:08 +0600

    Bug free university Management system for booster group presentation

---
+++ UniversityCourseandResultManagementSystem/Controllers/ViewResultController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Rotativa;
+using UniversityCourseandResultManagementSystem.Controllers;
+using UniversityCourseandResultManagementSystem.BLL;
+using UniversityCourseandResultManagementSystem.Models;
+
+namespace UniversityCourseandResultManagementSystem.Controllers
+{
+    public class ViewResultController : Controller
+    {
+        ViewResultManager viewResultManager=new ViewResultManager();
+          public ActionResult ViewResult()
+        {
+            ViewBag.StudentList = viewResultManager.GetStudentDropdownList();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ViewResult(Student student)
+        {
+ 
+            ViewBag.ViewResults = viewResultManager.GetResultList(student.ID);
+            ViewBag.StudentList = viewResultManager.GetStudentDropdownList();
+            return View();
+        }
+              [HttpPost]
+
+        public ActionResult ExportResult()
+           {
+            ViewBag.StudentList = viewResultManager.GetStudentDropdownList();
+            return View();
+        }
+    
+        public ActionResult ExportResult(int id)
+             {
+                
+            ViewBag.ViewResults = viewResultManager.GetResultList(id);
+            ViewBag.StudentList = viewResultManager.GetStudentDropdownList();
+            return View();
+        }
+
+      
+        public JsonResult GetStudentName(int id)
+        {
+            Student student = viewResultManager.GetStudentInformation(id);
+            string name = student.Name;
+            return Json(name);
+
+        }
+        public JsonResult GetStudentEmail(int id)
+        {
+            Student student = viewResultManager.GetStudentInformation(id);
+            string email = student.Email;
+            return Json(email);
+
+        }
+        public JsonResult GetStudentDepartment(int id)
+        {
+            Student student = viewResultManager.GetStudentInformation(id);
+            string department = student.Department;
+            return Json(department);
+
+        }
+
+        public ActionResult GeneratePDF(int id)
+        {
+            return new ActionAsPdf("ExportResult/"+id)
+            {
+                FileName  = Server.MapPath("~/Content/StudentResult.pdf")
+            };
+        }
+	}
+}
---
+++ UniversityCourseandResultManagementSystem/DAL/CourseStaticsGateway.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using UniversityCourseandResultManagementSystem.Models;
+
+namespace UniversityCourseandResultManagementSystem.DAL
+{
+    public class CourseStaticsGateway : CommonGateway
+    {
+        public List<CourseStatics> GetCourseStatics(string departmentCode)
+        {
+            List<CourseStatics> courseStaticsList = new List<CourseStatics>();
+            GenarateConnection();
+            using (Connection)
+            {
+                string query = "select * from CourseStatics where Department = @Department and Visible = @Visible;";
+                Command = new SqlCommand(query, Connection);
+                Command.Parameters.Clear();
+                Connection.Open();
+                Command.Parameters.Add("@Department", SqlDbType.VarChar);
+                Command.Parameters["@Department"].Value = departmentCode;
+                Command.Parameters.Add("@Visible", SqlDbType.Int);
+                Command.Parameters["@Visible"].Value = 1;
+
+
+                Reader = Command.ExecuteReader();
+                if (Reader.HasRows)
+                {
+                    while (Reader.Read())
+                    {
+                        CourseStatics courseStatics=new CourseStatics();
+                        courseStatics.Code = Reader["Code"].ToString();
+                        courseStatics.Name = Reader["Name"].ToString();
+                        courseStatics.Semester = Reader["Semester"].ToString();
+                        courseStatics.Teacher = Reader["Teacher"].ToString();
+                        courseStaticsList.Add(courseStatics);
+                    }
+                }
+                Connection.Close();
+            }
+            return courseStaticsList;
+        }
+        public List<Department> GetDepartmentDropdownList()
+        {
+            List<Department> departmentList = new List<Department>();
+            GenarateConnection();
+            using (Connection)
+            {
+                Connection.Open();
+
+                string query = "select * from Department;";
+                Command = new SqlCommand(query, Connection);
+                Reader = Command.ExecuteReader();
+                if (Reader.HasRows)
+                {
+                    while (Reader.Read())
+                    {
+                        Department department = new Department();
+                        department.Code = Reader["Code"].ToString();
+                        department.Name = Reader["Name"].ToString();
+                        departmentList.Add(department);
+                    }
+                }
+                Connection.Close();
+            }
+            return departmentList;
+        }
+    }
+}
